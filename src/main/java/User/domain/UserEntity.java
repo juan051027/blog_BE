eb69430d28e899cd532b42efd1dc9ea3012ac462
82177 <8217759juan@naver.com>
2024-08-26 22:01:16 +0900

    tt

--- src/main/java/User/domain/UserEntity.java
+++ src/main/java/User/domain/UserEntity.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.lang.String;
 
 @Entity
 @NoArgsConstructor
@@ -19,4 +20,17 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
